148e41ad4e7fe8982f9c058c3564564e0e953918
Maxime S <Maxmtlfr@gmail.com>
2023-04-02 18:16:12 +0200

    Grid, responsive, fotter

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,7 @@
 import { VisitCard } from '../Components/VisitCard/VisitCard';
 import { VisitCardProps } from '../Components/VisitCard/VisitCard';
 import { GridPics } from '../Components/GridPics/GridPics';
+import { Footer } from '../Components/Footer/Footer';
 import './_home.scss';
 export function Home() {
   const visitCardProps: VisitCardProps = {
@@ -15,6 +16,7 @@ export function Home() {
     <div className="home">
       <VisitCard {...visitCardProps} />
       <GridPics />
+	  <Footer />
     </div>
   );
 }
